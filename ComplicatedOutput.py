aa8a1d9de40bdd9aa365357c6eddb94d253cf7bc
skancey <siddiqkancey@gmail.com>
2017-04-28 16:05:50 -0400

    Add files via upload

--- ComplicatedOutput.py
+++ ComplicatedOutput.py
@@ -1,8 +1,2 @@
-name = 'Bob Newhard' #name
-payrate = '$25.25' #pay rate 
-pay = 25.25
-hourswrk = 45 #hours worked  
-
-totalpay = pay * hourswrk
-
-print name,"has worked", hourswrk,"hours at",payrate,"per hour for a total of $",totalpay,"pay."
+output = "FirstName LastName\nPay Rate: $X\nHours Worked: X\nTotal Pay: X\n"
+print output
