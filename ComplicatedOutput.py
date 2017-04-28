9dcac2dbbfa9aa6989c56b72a84a5c9261a99c6f
skancey <siddiqkancey@gmail.com>
2017-04-28 16:03:03 -0400

    Add files via upload

---
+++ ComplicatedOutput.py
@@ -0,0 +1,8 @@
+name = 'Bob Newhard' #name
+payrate = '$25.25' #pay rate 
+pay = 25.25
+hourswrk = 45 #hours worked  
+
+totalpay = pay * hourswrk
+
+print name,"has worked", hourswrk,"hours at",payrate,"per hour for a total of $",totalpay,"pay."
